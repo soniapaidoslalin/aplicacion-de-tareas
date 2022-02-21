1aacb277d3e5bc010a3ab95611d98798455bcd3a
sonia <sonia.paidoslalin@gmail.com>
2022-02-21 12:37:34 +0100

    añadir tarea en html

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,19 +1,23 @@
-import '../css/componentes.css';
-import webpacklogo from '../assets/img/webpack-logo.png';
+//Referencias HTML
 
+const divTodoList=document.querySelector('.todo-list');
 
-export const saludar =(nombre) =>{
-    console.log('Creando etiqueta h1');
+export const crearTodoHtml=(todo) =>{
+    const htmlTodo=`
+    <li class="${(todo.completado)?'completed':''}" data-id="${todo.id}">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${(todo.completado)?'checked':''}>
+            <label>${todo.tarea}</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li>`;
 
-    const h1=document.createElement('h1');
-    h1.innerText=`Hola!, feliz dia ${nombre}`;
+    const div=document.createElement('div');
+    div.innerHTML=htmlTodo;
 
-    document.body.append(h1);
+    divTodoList.append(div.firstElementChild);
 
-    //Img
+    return div.firstElementChild;
 
-    // console.log(webpacklogo);
-    // const img=document.createElement('img');
-    // img.src=webpacklogo;
-    // document.body.append(img);
 }
