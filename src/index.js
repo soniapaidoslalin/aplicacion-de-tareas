f27cf8ec5135eb57f56b641408be8a56ca7e6f86
sonia <sonia.paidoslalin@gmail.com>
2022-02-21 12:36:58 +0100

    añadir tarea en html

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import './styles.css';
 
 import { TodoList} from './classes/todo-list.class';
 import { Todo } from './classes/todo.class';
+import { crearTodoHtml } from './js/componentes';
 
 const todoList=new TodoList();
 
 const tarea=new Todo('Aprender JavaScript');
 
+
 todoList.nuevoTodo(tarea);
 
 console.log(todoList);
+
+
+
+crearTodoHtml(tarea);
