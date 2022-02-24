d6dec9023060c5f312a2aa32d8f21b5f071fd7c4
sonia <sonia.paidoslalin@gmail.com>
2022-02-24 13:00:18 +0100

    añadir filtros

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,9 +3,11 @@
 import { todoList } from "..";
 import { Todo } from "../classes/todo.class";
 
-const divTodoList  =document.querySelector('.todo-list');
-const txtInput     =document.querySelector('.new-todo');
-const btnBorrar    =document.querySelector('.clear-completed');
+const divTodoList   =document.querySelector('.todo-list');
+const txtInput      =document.querySelector('.new-todo');
+const btnBorrar     =document.querySelector('.clear-completed');
+const ulFiltros     =document.querySelector('.filters');
+const anchorFiltros =document.querySelectorAll('.filtro');
 
 export const crearTodoHtml=(todo) =>{
     const htmlTodo=`
@@ -67,4 +69,39 @@ btnBorrar.addEventListener('click',() => {
             divTodoList.removeChild(elemento);
         }
     }
+});
+
+ulFiltros.addEventListener('click',(event) =>{
+    const filtro=event.target.text;
+    if (!filtro){
+        return;
+    }
+
+    anchorFiltros.forEach(elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+    for (const elemento of divTodoList.children) {
+        elemento.classList.remove('hidden');
+        const completado=elemento.classList.contains('completed');
+
+        switch (filtro) {
+            case 'Pendientes':
+                if (completado){
+                    elemento.classList.add('hidden');
+                }
+                
+            break;
+            case 'Completados':
+                if (!completado){
+                    elemento.classList.add('hidden');
+                }
+                
+            break;
+        
+            
+        }
+    }
+        
+    
+    
 });
